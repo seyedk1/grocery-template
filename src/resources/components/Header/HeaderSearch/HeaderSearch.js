e7b8e7d56c86e92f45f8bd14ad4e9587f8c742c8
Keyvan <keyvan@Keyvans-MacBook-Pro.local>
2023-11-13 11:06:28 +0330

    add navigation to icons that user can navigate by this icon between pages

--- src/resources/components/Header/HeaderSearch/HeaderSearch.js
+++ src/resources/components/Header/HeaderSearch/HeaderSearch.js
@@ -5,16 +5,36 @@ import {
   mdiAccountOutline,
   mdiMagnify,
 } from "@mdi/js";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
   setup() {
-    const path = ref([mdiHeartOutline, mdiCartOutline, mdiAccountOutline]);
+    const router = useRouter();
+    const path = reactive([
+      {
+        icon: mdiHeartOutline,
+        to: "home",
+      },
+      {
+        icon: mdiCartOutline,
+        to: "cart",
+      },
+      {
+        icon: mdiAccountOutline,
+        to: "profile",
+      },
+    ]);
     const search = ref(mdiMagnify);
+
+    let navigateTo = (icon) => {
+      router.push({ name: icon.to });
+    };
 
     return {
       path,
-      search
+      search,
+      navigateTo,
     };
   },
 
--- src/resources/components/Header/HeaderSearch/HeaderSearch.vue
+++ src/resources/components/Header/HeaderSearch/HeaderSearch.vue
@@ -1,49 +1,52 @@
 <template>
-    <div id="header-main">
-      <div class="header-logo-parent">
-        <v-img
-          src="/src/assets/images/logo/logo.png"
-          width="70"
-          height="auto"
-          class="logo-image-style"
-        ></v-img>
-      </div>
-      <div class="search-parent">
-        <v-autocomplete
-        :menu-props="{
-          border:1 +'px solid #ced4da',
-          borderRadius:0.25+'rem'
-        }"
-          auto-select-first
-          class="placeholder-color-style"
-          density="comfortable"
-          item-props
-          menu-icon=""
-          placeholder="جستجوی محصول"
-          prepend-inner-icon="mdi mdi-magnify"
-          theme="light"
-          variant="solo"
-        >
-          <template #no-data> محصولی یافت نشد </template>
-        </v-autocomplete>
-      </div>
-      <div class="icons--language-parent">
-        <ul class="ul-icons">
-          <li class="icons-style-li" v-for="(icon, index) in path">
-            <svg-icon
-              type="mdi"
-              width="47"
-              height="47"
-              :path="icon"
-              :key="index"
-              class="icon-style"
-            ></svg-icon>
-          </li>
-        </ul>
-      </div>
+  <div id="header-main">
+    <div class="header-logo-parent">
+      <v-img
+        src="/src/assets/images/logo/logo.png"
+        width="70"
+        height="auto"
+        class="logo-image-style"
+      ></v-img>
     </div>
-  </template>
-  
-  <style src="./HeaderSearch.scss" lang="scss" scoped></style>
-  <script src="./HeaderSearch.js"></script>
-  
+    <div class="search-parent">
+      <v-autocomplete
+        :menu-props="{
+          border: 1 + 'px solid #ced4da',
+          borderRadius: 0.25 + 'rem',
+        }"
+        auto-select-first
+        class="placeholder-color-style"
+        density="comfortable"
+        item-props
+        menu-icon=""
+        placeholder="جستجوی محصول"
+        prepend-inner-icon="mdi mdi-magnify"
+        theme="light"
+        variant="solo"
+      >
+        <template #no-data> محصولی یافت نشد </template>
+      </v-autocomplete>
+    </div>
+    <div class="icons--language-parent">
+      <ul class="ul-icons">
+        <li
+          class="icons-style-li"
+          v-for="(icon, index) in path"
+          @click="navigateTo(icon)"
+        >
+          <svg-icon
+            type="mdi"
+            width="47"
+            height="47"
+            :path="icon.icon"
+            :key="index"
+            class="icon-style"
+          ></svg-icon>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style src="./HeaderSearch.scss" lang="scss" scoped></style>
+<script src="./HeaderSearch.js"></script>
