8464cdc45e60d8dbb2848acb0b58cb622f40247f
Keyvan <keyvan@Keyvans-MacBook-Pro.local>
2023-11-13 11:38:06 +0330

    add cart page to project

---
+++ src/views/Cart/ViewsCart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="main-container">
+    <Advertisement></Advertisement>
+    <HeaderSearch></HeaderSearch>
+    <HeaderMenu></HeaderMenu>
+    <PageHeader>سبد خرید</PageHeader>
+
+    <CartContainer></CartContainer>
+    <Footer></Footer>
+  </div>
+</template>
+
+<style src="./ViewsCart.scss" lang="scss"></style>
+<script src="./ViewsCart.js"></script>
